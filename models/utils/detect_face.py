efe5435e669000b5e17aae7fcb374b3bd8e185c8
Naufal Au <red.naufal.au@gmail.com>
2022-08-27 20:46:58 +0700

    mengambil rnet dgn mengubahnya ke list

--- models/utils/detect_face.py
+++ models/utils/detect_face.py
@@ -134,7 +134,7 @@ def detect_face(imgs, minsize, pnet, rnet, onet, threshold, factor, device):
         out = fixed_batch_process(im_data, rnet)
         
         # mengambil semua yang ingin kuketahui
-        loot['rnet'] = out.tolist()
+        loot['rnet'] = list(out)
         #
 
         out0 = out[0].permute(1, 0)
